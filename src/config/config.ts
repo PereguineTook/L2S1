e239819b00d01bf4c8803e16ff82a08d30d7186c
Chris <chris.p.bramwell@gmail.com>
2023-03-28 13:17:44 -0500

    Added unlockables

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,23 +21,10 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
+    cid: "bafybeidan4gd7o2myxv6nvhzyppposkquupint2ggamw3n26ujmjkzqjnm", // HTML blog example
     nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
+      "0xe64fff3c7d8e372ff5a1daefacb98322bb41c619442d56a310760865ac673291",
     ], // You need to own 'GM #1' to unlock this.
-  },
-  {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
-  },
-  {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
   },
 ];
 
